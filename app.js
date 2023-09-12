3c5b18a84a5f2c195a7938ac3f04ce68f5e1bdd2
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2023-09-12 16:31:46 +0400

    added every necessary middleware

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/errorHandler');
 require('dotenv').config();
 
+app.use(express.static('./public'));
 app.use(express.json());
 
 app.use('/api/v1/tasks', tasks);
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
